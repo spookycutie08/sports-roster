bc42d35189b0cfda71215c5f9c05160b3250ffbe
Bethany Whitworth <bethanyjwhitworth@gmail.com>
2020-05-18 21:36:10 -0500

    navbar, authentication complete with buttons

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import './Navbar.scss';
+
+class Navbar extends React.Component {
+  static propTypes = {
+    authed: PropTypes.bool.isRequired,
+  }
+
+  logMeOut = (e) => {
+    e.preventDefault();
+    firebase.auth().signOut();
+  };
+
+  render() {
+    const { authed } = this.props;
+    return (
+      <div className="Navbar">
+        <nav className="navbar navbar-expand-lg navbar-light bg-primary">
+          <a className="navbar-brand" href="#">Sports Roster</a>
+          <button className="navbar-toggler" type="button">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                {
+                  authed ? <button className="nav-link btn btn-light" onClick={this.logMeOut}>Logout</button>
+                    : ''
+                }
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
